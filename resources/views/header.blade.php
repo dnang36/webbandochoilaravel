1eab782d37cfd5483f3eb488cb15b3d39ea9072d
Admin <nguyendoandangndd@gmail.com>
2023-05-25 09:15:58 +0700

    update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,7 +8,7 @@
 
 					<!-- Logo desktop -->
 					<a href="#" class="logo">
-						<img src="/template/images/icons/KFC.png" alt="IMG-LOGO">
+						<img src="/template/images/icons/img.png" alt="IMG-LOGO">
 					</a>
 
 					<!-- Menu desktop -->
